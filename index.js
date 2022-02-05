b3599249909b4e9b6ef6b78231023041710c8e12
Lior <levydlior@gmail.com>
2022-02-05 15:16:52 -0700

    commit tree 9cc893b26860dc8e222b2ba8e26bc1e70421f4e0

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats= ['Milo', 'Otis', 'Garfield']
+function beforeEach(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+function destructivelyAppendCat(name){
+    cats.push(name); 
+} 
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+  const newCats= [...cats, "Broom"];  
+  return newCats
+}
+
+function prependCat(){
+const newCats2= ["Arnold", ...cats]
+return newCats2
+
+}
+
+function removeLastCat(){
+const cats3= cats.slice(0,2)
+return cats3
+}
+
+function removeFirstCat(){
+const cats4= cats.slice(1)
+return cats4
+}
